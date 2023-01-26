302aa8504106b8be956f533f93da2f0738e00245
Nadia <105260246+nadinshy@users.noreply.github.com>
2023-01-26 10:24:12 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     getTips()
   }, [])
     const getTips = async () => {
-      const response = await fetch (`http://www.boredapi.com/api/activity/ `);
+      const response = await fetch (`https://www.boredapi.com/api/activity/ `);
       const data = await response.json();
       setTips(data.activity);
     };
